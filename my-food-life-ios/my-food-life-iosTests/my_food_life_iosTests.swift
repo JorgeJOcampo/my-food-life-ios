bbde9cde708454230bbbced75a43b35d6e0b8f73
Jorge Ocampo <jorgejocampo@gmail.com>
2025-11-30 11:53:36 -0300

    feat: implement initial My Food Life game with core scenes, logic, and assets

---
+++ my-food-life-ios/my-food-life-iosTests/my_food_life_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  my_food_life_iosTests.swift
+//  my-food-life-iosTests
+//
+//  Created by Jorge Ocampo on 30/11/2025.
+//
+
+import Testing
+@testable import my_food_life_ios
+
+struct my_food_life_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
